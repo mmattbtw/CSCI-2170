496961a8fc4f60facf6d63b77e16ee5998f55cf7
mmattbtw <matt@mmatt.net>
2024-08-29 12:45:28 -0500

    so true

--- input.cpp
+++ input.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 
 int main() {
-	int age;
 
+	int age;
+	float gpa;
+	char letter;
+
+	std::cout << "Enter your age: ";
 	std::cin >> age;
 
-	std::cout << "AGE: " << age << "\n";
+
+	std::cout << "Enter your GPA: ";
+	std::cin >> gpa;
+
+	std::cout << "Enter your favorite letter: ";
+	std::cin >> letter;
+
+	std::cout << "Damn you old like " << age << " is crazyyyy..." << "\n";
+	std::cout << gpa << " FLOPPPPPPPPP" << "\n";
+	std::cout << letter << ", flop ass letter ijbol" << "\n";
 
 	return 0;
+
 }
